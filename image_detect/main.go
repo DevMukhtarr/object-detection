880a17fdde6663fce68632c8b6eb1f109ff1503b
Mukhtar <mukhtarabdulfatai@gmail.com>
2024-07-22 10:14:50 +0100

    detection sytem

---
+++ image_detect/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"cloud.google.com/go/translate"
+	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
+	"github.com/joho/godotenv"
+	"golang.org/x/text/language"
+	"google.golang.org/api/option"
+)
+
+type Detection struct {
+	Class      string  `json:"class"`
+	Confidence float64 `json:"confidence"`
+	Box        [4]int  `json:"box"`
+}
+
+type PythonResponse struct {
+	Detections     []Detection `json:"detections"`
+	AnnotatedImage string      `json:"annotated_image"`
+}
+
+type DetectionRequest struct {
+	Image string `json:"image"`
+}
+
+type TranslateRequest struct {
+	Detections []string `json:"detections"`
+	Lang       string   `json:"lang"`
+}
+
+var translateClient *translate.Client
+
+func setupGoogleTranslate() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Printf("Error loading .env file: %v\n", err)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	var err error
+
+	credsJSON := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+	var credsMap map[string]interface{}
+
+	if err := json.Unmarshal([]byte(credsJSON), &credsMap); err != nil {
+		fmt.Printf("Error parsing service account JSON: %v\n", err)
+		os.Exit(1)
+	}
+	credsBytes, err := json.Marshal(credsMap)
+
+	if err != nil {
+		fmt.Printf("Error converting service account to bytes: %v\n", err)
+		os.Exit(1)
+	}
+
+	translateClient, err = translate.NewClient(ctx, option.WithCredentialsJSON(credsBytes))
+	if err != nil {
+		fmt.Printf("Failed to create client: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func translateText(detections []string, lang string) ([]string, error) {
+	ctx := context.Background()
+	translations := make([]string, len(detections))
+	langTag := language.Make(lang)
+	for i, detection := range detections {
+		resp, err := translateClient.Translate(ctx, []string{detection}, langTag, nil)
+		if err != nil {
+			return nil, err
+		}
+		if len(resp) > 0 {
+			translations[i] = resp[0].Text
+		} else {
+			translations[i] = detection
+		}
+	}
+	return translations, nil
+	// // target, err := language.Parse(targetLang)
+	// if err != nil {
+	// 	return "", fmt.Errorf("failed to parse target language: %v", err)
+	// }
+
+	// resp, err := translateClient.Translate(ctx, []string{text}, target, nil)
+	// if err != nil {
+	// 	return "", fmt.Errorf("failed to translate text: %v", err)
+	// }
+
+	// return resp[0].Text, nil
+}
+
+func detectObjects(imageData string) ([]PythonResponse, error) {
+	client := resty.New()
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(DetectionRequest{Image: imageData}).
+		Post("http://localhost:5000/detect")
+
+	if err != nil {
+		return nil, err
+	}
+
+	var pyResp PythonResponse
+	if err := json.Unmarshal(resp.Body(), &pyResp); err != nil {
+		return nil, err
+	}
+
+	return []PythonResponse{pyResp}, nil
+}
+
+func main() {
+	setupGoogleTranslate()
+	r := gin.Default()
+
+	r.Static("/static", "./static")
+
+	r.GET("/", func(c *gin.Context) {
+		c.File("./static/index.html")
+	})
+
+	r.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile("image")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Image upload failed"})
+			return
+		}
+
+		fileData, err := file.Open()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to open image file"})
+			return
+		}
+		defer fileData.Close()
+
+		buffer := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buffer, fileData); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read image file"})
+			return
+		}
+
+		encodedImage := base64.StdEncoding.EncodeToString(buffer.Bytes())
+		detections, err := detectObjects(encodedImage)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Object detection failed"})
+			return
+		}
+		c.JSON(http.StatusOK, detections)
+	})
+
+	r.POST("/translate", func(c *gin.Context) {
+		var translateReq TranslateRequest
+		if err := c.ShouldBindJSON(&translateReq); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+			return
+		}
+
+		translations, err := translateText(translateReq.Detections, translateReq.Lang)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Translation failed"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"translations": translations})
+	})
+
+	r.Run(":8080")
+}
---
+++ yolo_service.py
@@ -0,0 +1,58 @@
+# yolo_service.py
+from ultralytics import YOLO
+from flask import Flask, request, jsonify
+import base64
+from PIL import Image
+from io import BytesIO
+from dotenv import load_dotenv
+import cloudinary
+from cloudinary import CloudinaryImage
+import cloudinary.uploader
+import cloudinary.api
+import os
+import json
+
+load_dotenv()
+
+app = Flask(__name__)
+model = YOLO('./yolov8n.pt')  # Load a pre-trained YOLOv8 model
+
+config = cloudinary.config(secure=True)
+@app.route('/detect', methods=['POST'])
+def detect_objects():
+    data = request.json
+    image_data = base64.b64decode(data['image'])
+    image = Image.open(BytesIO(image_data))
+    results = model(image)
+    detections = []
+    
+    buffer = BytesIO()
+
+    for result in results:
+        annotated_image_array = result.plot()
+        annotated_image = Image.fromarray(annotated_image_array)
+        annotated_image.save(buffer, format='JPEG') 
+
+    buffer.seek(0)
+
+    upload_response =  cloudinary.uploader.upload(buffer, public_id="detected_image", unique_filename = True, overwrite=True)
+    srcURL = upload_response['secure_url']
+
+    for result in results:
+        boxes = result.boxes.xyxy.cpu().numpy()  
+        scores = result.boxes.conf.cpu().numpy() 
+        class_ids = result.boxes.cls.cpu().numpy() 
+
+        for box, score, class_id in zip(boxes, scores, class_ids):
+            detections.append({
+                'class': result.names[int(class_id)],
+                'confidence': float(score),
+                'box': [int(coord) for coord in box]
+            })
+    return jsonify({ 
+                    "detections": detections,
+                     "annotated_image": srcURL 
+     })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
